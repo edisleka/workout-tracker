289a3bc4c4d7470ee1d39963a46c1c21b45ee025
Edis Leka <edisleka@yahoo.gr>
2025-07-02 19:16:20 +0300

    clerk setup & sign in screen completed

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,9 +1,34 @@
+import { useAuth } from '@clerk/clerk-expo'
 import { Stack } from 'expo-router'
+import { ActivityIndicator, View } from 'react-native'
 
 export default function AppLayout() {
+  const { isSignedIn, isLoaded, userId, sessionId, getToken } = useAuth()
+
+  console.log('isSignedIn ->', isSignedIn)
+  console.log('isLoaded ->', isLoaded)
+  console.log('userId ->', userId)
+  console.log('sessionId ->', sessionId)
+  console.log('getToken ->', getToken())
+
+  if (!isLoaded) {
+    return (
+      <View className='flex-1 items-center justify-center'>
+        <ActivityIndicator size='large' color='green' />
+      </View>
+    )
+  }
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='(tabs)' />
+    <Stack>
+      <Stack.Protected guard={!!isSignedIn}>
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name='sign-in' options={{ headerShown: false }} />
+        <Stack.Screen name='sign-up' options={{ headerShown: false }} />
+      </Stack.Protected>
     </Stack>
   )
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,11 @@
 import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Slot } from 'expo-router'
 import '../../global.css'
 
 export default function RootLayout() {
   return (
-    <ClerkProvider>
+    <ClerkProvider tokenCache={tokenCache}>
       <Slot />
     </ClerkProvider>
   )
